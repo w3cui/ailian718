cf7a525e42b707ab8414d3d117a62a7a3103e7a3
[夜 <[tql163.love@163.com]>
2018-05-31 15:59:21 +0800

    修改

---
+++ build/www/lib/wui/js/modules/box.js
@@ -0,0 +1,5 @@
+/**
+ *  - v1.0.0  License By 
+ * 研发技术中心-技术研发部 
+ */
+!function(i,n){alert("我是弹窗组件！")}(window,lwjui);
---
+++ build/www/lib/wui/js/modules/layer.js
@@ -0,0 +1,5 @@
+/**
+ *  - v1.0.0  License By 
+ * 研发技术中心-技术研发部 
+ */
+!function(l,e){"use strict";l.layer={},l.layer.host=e.cache.dir+"plugin/layer/",e.loadAll({uses:["plugin/layer/layer.js"]},function(){e.layer=layer})}(window,wui);
